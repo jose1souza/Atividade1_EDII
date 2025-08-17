1385ce7947b01d7df506da36a207e08367c1d6df
jose1.souza <zexnineestudos@gmail.com>
2025-08-17 15:17:09 -0300

    feat: function sortFlag and CompareTo dataBirth for Person

--- src/App.java
+++ src/App.java
@@ -1,36 +1,49 @@
 import java.util.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 
 public class App {
-	 public static Pessoa[] vetorDePessoas() {
-	        return new Pessoa[]{
-	            new Pessoa("João", LocalDate.of(1990, 1, 1)),
-	            new Pessoa("Ana", LocalDate.of(1992, 2, 2)),
-	            new Pessoa("Maria", LocalDate.of(1995, 5, 5)),
-	            new Pessoa("Pedro", LocalDate.of(1993, 4, 4)),
-	            new Pessoa("Lucas", LocalDate.of(1991, 6, 6)),
-	            new Pessoa("Carlos", LocalDate.of(1988, 3, 3))
-	        };
-	    }
-	 public static Integer[] carregarArquivo(String nomeArquivo) throws IOException {
-	        List<String> linhas = Files.readAllLines(Paths.get(nomeArquivo));
-	        Integer[] numeros = new Integer[linhas.size()];
-	        for (int i = 0; i < linhas.size(); i++) {
-	            numeros[i] = Integer.parseInt(linhas.get(i).trim());
-	        }
-	        return numeros;
-	    }
+	public static Pessoa[] vetorDePessoas() {
+		return new Pessoa[] { new Pessoa("João", LocalDate.of(1990, 1, 1)), new Pessoa("Ana", LocalDate.of(1992, 2, 2)),
+				new Pessoa("Maria", LocalDate.of(1995, 5, 5)), new Pessoa("Pedro", LocalDate.of(1993, 4, 4)),
+				new Pessoa("Lucas", LocalDate.of(1991, 6, 6)), new Pessoa("Carlos", LocalDate.of(1988, 3, 3)) };
+	}
+
+	public static Integer[] carregarArquivo(String nomeArquivo) throws IOException {
+		List<String> linhas = Files.readAllLines(Paths.get(nomeArquivo));
+		Integer[] numeros = new Integer[linhas.size()];
+		for (int i = 0; i < linhas.size(); i++) {
+			numeros[i] = Integer.parseInt(linhas.get(i).trim());
+		}
+		return numeros;
+	}
+
 	static Scanner leia = new Scanner(System.in); // instância do Scanner
+
 	public static void main(String[] args) throws Exception {
+		Integer[] arquivoCarregado = carregarArquivo("vetor_ordenado.txt");
+
+		System.out.println("Vetor antes de ordenar:: " + Arrays.toString(arquivoCarregado));
+
+		BubbleSort<Integer> bsortInteger = new BubbleSort<>();
+		bsortInteger.sort(arquivoCarregado);
+		System.out.println("Vetor depois de ordenar:" + Arrays.toString(arquivoCarregado));
+
 		Pessoa[] vetorPessoas = vetorDePessoas();
-		System.out.println("Pessoas antes de ordenar: "+ 
-		        Arrays.toString(vetorPessoas));
-		
+		System.out.println("Pessoas antes de ordenar: " + Arrays.toString(vetorPessoas));
+
 		BubbleSort<Pessoa> bsortPessoa = new BubbleSort<>();
 		// Criar comparator por data de nascimento
 		bsortPessoa.sort(vetorPessoas);
-		System.out.println("Pessoas depois de ordenar: "+ 
-				Arrays.toString(vetorPessoas));
-		
+		System.out.println("Pessoas depois de ordenar: " + Arrays.toString(vetorPessoas));
+
 	} // fim do main
 } // fim da classe App
+
+/* Número 2 da atividade
+  	Fases: 87
+  	Comparations: 4872
+	Exchanges: 2343
+  */
--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,50 +1,83 @@
 import java.util.*;
 
 public class BubbleSort<T> {
-	private void troca(T[] array, int i, int j){
-        T temporaria = array[i]; // armazena temporariamente
-        array[i] = array[j]; // substitui os valores de i e j
-        array[j] = temporaria;
-	} // fim do metódo troca  
-	
+	private void troca(T[] array, int i, int j) {
+		T temporaria = array[i]; // armazena temporariamente
+		array[i] = array[j]; // substitui os valores de i e j
+		array[j] = temporaria;
+	} // fim do metódo troca
+
 	public T[] sort(T[] array, Comparator<T> comparator) {
-		int n = array.length; // este é o n
-		for(int fase=1;fase<n;fase++) {
-			for(int comparacoes=0;comparacoes<n-fase;comparacoes++) { // percorre até o ultimo que não está organizado
-				if(comparator.compare(array[comparacoes], array[comparacoes+1]) > 0) { // compara se o primeiro valor é maior que o próximo
-					troca(array,comparacoes,comparacoes+1); // troca se for maior
+		int numberOfElements = array.length; // este é o n
+		for (int fase = 1; fase < numberOfElements; fase++) {
+			for (int comparacoes = 0; comparacoes < numberOfElements - fase; comparacoes++) { // percorre até o ultimo que não está
+																				// organizado
+				if (comparator.compare(array[comparacoes], array[comparacoes + 1]) > 0) { // compara se o primeiro valor
+																							// é maior que o próximo
+					troca(array, comparacoes, comparacoes + 1); // troca se for maior
 				} // fim do if
 			} // fim do for de comparacoes
 		} // fim do for da fase
 		return array;
 	} // fim do metódo sort
-	
-	public T[] sort(T[] array, Comparator<T> comparator, boolean verbose) { // metódo para caso o queira que detalhe as fases
+
+	public T[] sort(T[] array, Comparator<T> comparator, boolean verbose) { // metódo para caso o queira que detalhe as
+																			// fases
 		Scanner leia = new Scanner(System.in);
-		if(verbose==true) {
-		int n = array.length; // este é o n
-		for(int fase=1;fase<n;fase++) {
-			System.out.println("Fase: "+fase); // mostrando a cada fase
-			System.out.println(Arrays.toString(array));// como o vetor está durante a fase
-			leia.nextLine(); // pause
-			for(int comparacoes=0;comparacoes<n-fase;comparacoes++) { // percorre até o ultimo que não está organizado
-				if(comparator.compare(array[comparacoes], array[comparacoes+1]) > 0) { // compara se o primeiro valor é maior que o próximo
-					troca(array,comparacoes,comparacoes+1); // troca se for maior
-				} // fim do if
-			} // fim do for de comparacoes
-		} // fim do for da fase
-	} // fim do if de verbose
+		if (verbose == true) {
+			int numberOfElements = array.length; // este é o n
+			for (int fase = 1; fase < numberOfElements; fase++) {
+				System.out.println("Fase: " + fase); // mostrando a cada fase
+				System.out.println(Arrays.toString(array));// como o vetor está durante a fase
+				leia.nextLine(); // pause
+				for (int comparacoes = 0; comparacoes < numberOfElements - fase; comparacoes++) { // percorre até o ultimo que não está
+																					// organizado
+					if (comparator.compare(array[comparacoes], array[comparacoes + 1]) > 0) { // compara se o primeiro
+																								// valor é maior que o
+																								// próximo
+						troca(array, comparacoes, comparacoes + 1); // troca se for maior
+					} // fim do if
+				} // fim do for de comparacoes
+			} // fim do for da fase
+		} // fim do if de verbose
 		return array;
 	} // fim do metódo sort
 	
+	public T[] sortFlag(T[] array, Comparator<T> comparator, boolean verbose) {
+		Scanner leia = new Scanner(System.in);
+		int exchanges = 0, numberComparations = 0;
+
+		if (verbose == true) {
+			int numberOfElements = array.length;
+			for (int fase = 1; fase < numberOfElements; fase++) {
+				boolean exchange = false;
+				System.out.println("Fase: " + fase); // mostrando a cada fase
+				System.out.println(Arrays.toString(array));// como o vetor está durante a fase
+				leia.nextLine(); // pause
+				for (int comparacoes = 0; comparacoes < numberOfElements - fase; comparacoes++) {
+					numberComparations = numberComparations + 1;
+					if (comparator.compare(array[comparacoes], array[comparacoes + 1]) > 0) {
+						troca(array, comparacoes, comparacoes + 1);
+						exchange = true;
+						exchanges = exchanges + 1;
+					}
+				}
+				if (exchange == false && exchanges == 0) {
+					System.out.println("Este vetor já está ordenado!");
+					break;
+				}
+			}
+		}
+		System.out.println("Comparations: " + numberComparations);
+		System.out.println("Exchanges: " + exchanges);
+		return array;
+	}
+
 	public T[] sort(T[] array) { // Este vai pegar o comparator e passar para o sort independente de seu tipo
-		return sort(array, (a,b)->((Comparable<T>) a).compareTo(b));
+		return sortFlag(array, (a, b) -> ((Comparable<T>) a).compareTo(b),true);
 	}
 } // fim da classe
 
-/* Comparator retorna 
- 0 quando iguais
- -1 para menores 
- e 1 para maiores
+/*
+ * Comparator retorna 0 quando iguais -1 para menores e 1 para maiores
  */
- 
